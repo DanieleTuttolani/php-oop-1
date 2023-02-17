2e0d8fba2b9a8c0c739e6696af43a06b9d46d1d9
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-02-17 16:06:04 +0100

    function crated

--- index.php
+++ index.php
@@ -2,13 +2,24 @@
 class Movie{
     public $title;
     public $author;
+    public $genre;
 
 
-    public function __construct($title , $author){
+    public function __construct($title , $author , $genre){
         $this->title = $title;
         $this->author = $author;
+        $this->genre = $genre;
+    }
+    public function print_genre_title(){
+    echo $this->title . ' ' . $this->genre;
+
     }
 
 }
-$film1 = new Movie ('The truman show' , 'Daniele Tuttolani');
-var_dump($film1);
+$film1 = new Movie ('The truman show' , 'Daniele Tuttolani', 'Drama');
+$film2 = new Movie ('La vendetta agricola' , 'Daniele Tuttolani', 'Action');
+$film3 = new Movie ('Come diventare esperti a suonare l\'armonica' , 'Daniele Tuttolani', 'Romantic');
+
+$film3 ->print_genre_title();
+$film2 ->print_genre_title();
+$film1 ->print_genre_title();
