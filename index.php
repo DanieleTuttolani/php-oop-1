bc2d4778810c32eab2787e4200399b099c84ef50
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-02-17 15:44:39 +0100

    my first php object

--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 class Movie{
     public $title;
     public $author;
+
+
+    public function __construct($title , $author){
+        $this->title = $title;
+        $this->author = $author;
+    }
+
 }
+$film1 = new Movie ('The truman show' , 'Daniele Tuttolani');
+var_dump($film1);
